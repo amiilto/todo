63d30641539d860d142a18e33239406be5575033
Amilto Romagno <39378921+amiilto@users.noreply.github.com>
2022-04-14 15:32:56 -0300

    Containers, labels and checkboxes

---
+++ src/components/ListItem/styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    background-color: #20212C;
+    padding: 10px;
+    border-radius: 10px;
+    margin-bottom: 10px;
+    align-items: center;
+
+    label {
+        color: #CCC;
+    }
+
+    input {
+        width: 20px;
+        height: 20px;
+        margin-right: 5px;
+    }
+`;
